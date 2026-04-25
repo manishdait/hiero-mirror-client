1cac306ae96ff56c9fb9fc70739a584e9868e7d9
Manish Dait <daitmanish88@gmail.com>
2026-04-25 22:43:48 +0530

    feat: added the http client
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

---
+++ src/main/java/io/github/manishdait/mirrornodeclientj/core/RestClient.java
@@ -0,0 +1,48 @@
+package io.github.manishdait.mirrornodeclientj.core;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.jspecify.annotations.NonNull;
+
+public class RestClient {
+  private static final HttpClient client = HttpClient.newHttpClient();
+
+  static MirrorNodeResponse send(@NonNull final MirrorNodeRequest request)
+      throws IOException, InterruptedException {
+    Objects.requireNonNull(request, "request must not be null");
+    HttpRequest httpRequest =
+        HttpRequest.newBuilder().uri(URI.create(parseUrl(request))).GET().build();
+
+    HttpResponse<String> httpResponse =
+        client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+    return new MirrorNodeResponse(httpResponse.statusCode(), httpResponse.body());
+  }
+
+  private static String parseUrl(MirrorNodeRequest request) {
+    if (request.getQueryParams().isEmpty()) {
+      return request.getUrl();
+    }
+
+    String queryParams =
+        request.getQueryParams().entrySet().stream()
+            .flatMap(
+                entry ->
+                    entry.getValue().stream()
+                        .map(
+                            value ->
+                                URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8)
+                                    + "="
+                                    + URLEncoder.encode(value, StandardCharsets.UTF_8)))
+            .collect(Collectors.joining("&"));
+
+    return request.getUrl() + "?" + queryParams;
+  }
+}
--- src/test/java/io/github/manishdait/mirrornode_clientj/MirrorNodeClientTest.java
+++
@@ -1,4 +0,0 @@
-package io.github.manishdait.mirrornode_clientj;
-
-public class MirrorNodeClientTest {
-}
